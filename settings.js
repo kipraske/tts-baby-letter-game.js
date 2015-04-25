116b252be6be593bb6ee3910f2c5970986eb8706
Kip Raske <kip.raske@gmail.com>
2015-04-25 16:05:12 -0500

    Adding settings file which will determine the behavior of our application here.

---
+++ settings.js
@@ -0,0 +1,19 @@
+module.exports = {
+
+    // If set to true, the application can be closed with ctrl+c.
+    // Otherwise the application cannot be quit from inside the
+    // program (which is nice if your user is a baby)
+    canQuit: true,
+
+    // If set to true, you can change the settings from inside the program
+    // Controls:
+    canChangeSettings: true,
+
+    // Which voice you want to use. null will be the default festival voice
+    defaultVoice: null,
+
+    // If set to true, we will keep a history of letters typed which will
+    // all be read when you hit enter
+    wordMode: true
+};
+
