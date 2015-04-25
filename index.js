37ada667819a7f45b42d5100d6c0987bf7ea5161
Kip Raske <kip.raske@gmail.com>
2015-04-25 16:20:14 -0500

    Adding our functions that we will need in there. Design part 1

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 var say = require('say');
 var keypress = require('keypress');
 
+function checkQuit(key){
+    if (key && key.ctrl && key.name === 'c') {
+	process.stdin.pause();
+    }
+}
+
+function checkSettingChange(key){
+
+}
+
+function changeSetting(){
+
+}
+
+function speakLetter(ch){
+    console.log(ch);
+}
+
+function checkSpeakWords(key){
+
+}
+
+function speakWords(){
+    
+}
+
 keypress(process.stdin);
 process.stdin.setRawMode(true);
 
 process.stdin.on('keypress', function (ch, key) {
-
-    console.log(ch);
-    console.log(key);
-    console.log('\n');
-
-    if (key && key.ctrl && key.name === 'c') {
-	process.stdin.pause();
-    }
+    console.log(ch, key);
 });
