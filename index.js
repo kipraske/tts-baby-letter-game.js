0fde8619d312b73e67f9597e7062a672d2ccef50
Kristofer Raske <kristofer.raske@wustl.edu>
2015-05-01 16:33:51 -0500

    Of course, it is really inefficent to be loading up the words file again and again, so I am going to just cache it here in memory at the beginning of it all.  So much faster.

--- index.js
+++ index.js
@@ -2,13 +2,11 @@ var settings = require('./settings');
 var say = require('say');
 var keypress = require('keypress');
 var fs = require('fs');
+var cachedFullDictionary = '';
 var wordsBuffer = '';
 var speakingLock = false;
 var speakLetter = '';
 var dictonaryWord = '';
-
-keypress(process.stdin);
-process.stdin.setRawMode(true);
 
 if (settings.silentMode) {
 	say.speak = function (voice, text, callback) {
@@ -29,15 +27,11 @@ function checkQuit(key) {
 	return (key && key.ctrl && key.name === 'c');
 }
 
-function selectRandomDictionaryWord(ch, then) {
-	fs.readFile(settings.dictionaryPath, function(err, data){
-		var allWords = data.toString();
-		var firstLetterRegex = new RegExp('^' + ch + '.*$', 'igm');
-		var letterWords = allWords.match(firstLetterRegex); //letter words is an array of matches
-		var randIndex = Math.floor((Math.random() * letterWords.length));
-	    dictonaryWord = letterWords[randIndex];
-		then();
-	});
+function selectRandomDictionaryWord(ch) {
+	var firstLetterRegex = new RegExp('^' + ch + '.*$', 'igm');
+	var letterWords = cachedFullDictionary.match(firstLetterRegex); //letter words is an array of matches
+	var randIndex = Math.floor((Math.random() * letterWords.length));
+	dictonaryWord = letterWords[randIndex];
 }
 
 function speakLetterAndDictonaryWord() {
@@ -55,14 +49,14 @@ function checkSpeakableCharacter(ch) {
 }
 
 function doneSpeakingCharacter(err) {
-	if (err){
+	if (err) {
 		console.log(err);
 	}
 
 	speakingLock = false;
 }
 
-function prepareStateToSpeakCharacter(ch){
+function prepareStateToSpeakCharacter(ch) {
 	if (speakingLock) {
 		return;
 	}
@@ -92,27 +86,49 @@ function checkSpeakWords(key) {
 	return (key && key.name === 'return');
 }
 
-process.stdin.on('keypress', function (ch, key) {
-	if (checkQuit(key)) {
-		process.exit();
-	}
-	if (checkSpeakableCharacter(ch)) {
-		prepareStateToSpeakCharacter(ch);
-		if (settings.dictionaryMode){
-			selectRandomDictionaryWord(ch, speakLetterAndDictonaryWord);
-		}
-		else{
-			say.speak(settings.voice, speakLetter, doneSpeakingCharacter);
-		}
-		addCharToWords(ch);
-	}
-	if (ch === ' ') {
-		addCharToWords(ch);
-	}
-	if (checkSpeakWords(key)) {
-		say.speak(settings.voice, wordsBuffer);
-		wordsBuffer = '';
-	}
-});
+function loadMainKeypressHandler() {
+	keypress(process.stdin);
+	process.stdin.setRawMode(true);
 
-console.log('Now listening for keypresses...');
+	process.stdin.on('keypress', function (ch, key) {
+		if (checkQuit(key)) {
+			process.exit();
+		}
+		if (checkSpeakableCharacter(ch)) {
+			prepareStateToSpeakCharacter(ch);
+			if (settings.dictionaryMode) {
+				selectRandomDictionaryWord(ch);
+				speakLetterAndDictonaryWord();
+			}
+			else {
+				say.speak(settings.voice, speakLetter, doneSpeakingCharacter);
+			}
+			addCharToWords(ch);
+		}
+		if (ch === ' ') {
+			addCharToWords(ch);
+		}
+		if (checkSpeakWords(key)) {
+			say.speak(settings.voice, wordsBuffer);
+			wordsBuffer = '';
+		}
+	});
+
+	console.log('Now listening for keypresses.');
+}
+
+function cacheAllDictionaryWords(next){
+	console.log('Loading Dictionary...');
+	fs.readFile(settings.dictionaryPath, function (err, data) {
+		cachedFullDictionary = data.toString();
+		console.log('Dictionary Loaded!');
+		next();
+	});
+}
+
+if (settings.dictionaryMode){
+	cacheAllDictionaryWords(loadMainKeypressHandler);
+}
+else{
+	loadMainKeypressHandler();
+}
